4ff14989c2d23e3feece7575e2a86d836ca3cfad
Janosch Gräf <janosch@nimiq.com>
2021-04-14 02:23:07 +0200

    Improved example

--- src/lib.rs
+++ src/lib.rs
@@ -19,23 +19,28 @@
 //! ```rust
 //! use gv100ad::{
 //!     model::{
-//!         land::{LandDaten, LandSchluessel},
-//!         kreis::KreisDaten,
 //!         gemeinde::GemeindeDaten,
+//!         kreis::KreisDaten,
+//!         land::{LandDaten, LandSchluessel},
 //!     },
 //!     Database,
 //! };
-//!
+//! 
+//! // Open the database. Refer to the `README.md` file for the source of the datasets.
 //! let db = Database::from_path("GV100AD3004/GV100AD_300421.txt").unwrap();
-//!
+//! 
+//! // Parse a key for the state of Saarland
 //! let schluessel = "10".parse::<LandSchluessel>().unwrap();
+//! 
+//! // Get the record for the state of Saarland
 //! let land = db.get::<_, LandDaten>(schluessel).unwrap();
-//!
 //! println!("{}:", land.name);
-//!
+//! 
+//! // Iterate over the districts (Kreise) in Saarland
 //! for kreis in db.children::<_, KreisDaten>(schluessel) {
 //!     println!("  {}:", kreis.name);
-//!
+//! 
+//!     // Iterate over the municipalities (Gemeinde) in the district (Kreis)
 //!     for gemeinde in db.children::<_, GemeindeDaten>(kreis.schluessel) {
 //!         println!(
 //!             "    {}: {} residents",
@@ -43,6 +48,12 @@
 //!         );
 //!     }
 //! }
+//! 
+//! // Get the sum of the population of all municipalities in Saarland
+//! let total_population: u64 = db.children::<_, GemeindeDaten>(schluessel)
+//!     .map(|gemeinde| gemeinde.population_total)
+//!     .sum();
+//! println!("Total population of {}: {}", land.name, total_population);
 //! ```
 //!
 //! ## Language
